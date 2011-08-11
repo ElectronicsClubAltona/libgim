caa1f3312b61ae1f7e2a037f6e36bf8c2e385099
Danny Robson <danny@nerdcruft.net>
2011-08-12 00:25:59 +1000

    Added simple string routine files

---
+++ string.cpp
@@ -0,0 +1,30 @@
+/*
+ * This file is part of libgim.
+ *
+ * libgim is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * libgim is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libgim.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2011 Danny Robson <danny@blubinc.net>
+ */
+
+#include "string.hpp"
+
+#include <cstring>
+
+// TODO: Horribly inefficient, but God help you if you're relying on this
+// being efficient in the first place.
+bool
+strbegins (const char *restrict str,
+           const char *restrict prefix) {
+    return 0 == strncmp (prefix, str, strlen (prefix));
+}
