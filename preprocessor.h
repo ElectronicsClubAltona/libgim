a9f34ee449ebdb7e0bf137c4c52c3e262a535454
Danny Robson <danny@nerdcruft.net>
2012-04-19 16:42:35 +1000

    Add a header for simple preprocessor helpers

---
+++ preprocessor.h
@@ -0,0 +1,26 @@
+/*
+ * This file is part of libgim.
+ *
+ * libgim is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * libgim is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with libgim.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2012 Danny Robson <danny@blubinc.net>
+ */
+
+#ifndef __PREPROCESSOR_HPP
+#define __PREPROCESSOR_HPP
+
+#define CONCATENATE_DETAIL(x, y) x##y
+#define CONCATENATE(x, y) CONCATENATE_DETAIL(x, y)
+
+#endif
