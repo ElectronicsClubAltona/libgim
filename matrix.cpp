3d8213e00cfc713b1a9674a7b4e8945b4bb02b1a
Danny Robson <danny@nerdcruft.net>
2012-05-23 17:04:46 +1000

    Change licence header to match libgim default

--- matrix.cpp
+++ matrix.cpp
@@ -1,14 +1,21 @@
- /****************************************************************************
-                              __         .__  .__  .__                  .___
-   ___________ ___.__. _______/  |______  |  | |  | |__| ______ ____   __| _/
- _/ ___\_  __ <   |  |/  ___/\   __\__  \ |  | |  | |  |/  ___// __ \ / __ |
- \  \___|  | \/\___  |\___ \  |  |  / __ \|  |_|  |_|  |\___ \\  ___// /_/ |
-  \___  >__|   / ____/____  > |__| (____  /____/____/__/____  >\___  >____ |
-      \/       \/         \/            \/                  \/     \/     \/
-
-  Copyright:
-    Danny Robson, 2011
- *****************************************************************************/
+/*
+ * This file is part of libgim.
+ *
+ * libgim is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * libgim is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with libgim.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2011-2012 Danny Robson <danny@nerdcruft.net>
+ */
 
 #include "matrix.hpp"
 
