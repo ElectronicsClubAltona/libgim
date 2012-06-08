9098ea408de728a849e0582c3a55894edcbfb502
Danny Robson <danny@nerdcruft.net>
2012-06-08 16:48:33 +1000

    Add contains vs includes region tests

--- test/region.cpp
+++ test/region.cpp
@@ -1,7 +1,9 @@
 #include "../region.hpp"
+#include "../point.hpp"
 #include "../debug.hpp"
 
 using util::region;
+using util::point2;
 
 int
 main (int, char **) {
@@ -11,6 +13,17 @@ main (int, char **) {
 
          CHECK_HARD (!a.overlaps (b));
     }
+
+    CHECK_HARD (region<int> (0, 0, 2, 2).includes (point2(1.0, 1.0)));
+    CHECK_HARD (region<int> (0, 0, 2, 2).includes (point2(0.0, 0.0)));
+    CHECK_HARD (region<int> (0, 0, 2, 2).includes (point2(2.0, 2.0)));
+
+    CHECK_HARD ( region<int> (0, 0, 2, 2).contains (point2(1.0, 1.0)));
+    CHECK_HARD (!region<int> (0, 0, 2, 2).contains (point2(0.0, 0.0)));
+    CHECK_HARD (!region<int> (0, 0, 2, 2).contains (point2(2.0, 2.0)));
+
+    CHECK_HARD (region<intmax_t> (0, 0, 10, 10).includes (point2 (0.4, 0.01)));
+    CHECK_HARD (region<intmax_t> (0, 0, 10, 10).contains (point2 (0.4, 0.01)));
 
     return 0;
 }
