f3733b1cbc71f07efcc05500cfbe59d4b035b494
Danny Robson <danny@nerdcruft.net>
2014-12-16 15:29:49 +1100

    types: remove internal make_unique implementation
    we can use the c++1y implementation instead which is a lot more robust.

--- types.hpp
+++ types.hpp
@@ -32,13 +32,6 @@ constexpr size_t elems(const T (&)[N])
     { return N; }
 
 
-template <class T, class...Args>
-std::unique_ptr<T>
-make_unique(Args&&... args) {
-    return std::unique_ptr<T> (new T(std::forward<Args>(args)...));
-}
-
-
 //-----------------------------------------------------------------------------
 template <class T>
 T
