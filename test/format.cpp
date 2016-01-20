0fd72f0893d06782f3071bd50b8b83dc7492e9f2
Danny Robson <danny@nerdcruft.net>
2016-01-20 16:39:20 +1100

    format: test case whitespace

--- test/format.cpp
+++ test/format.cpp
@@ -13,9 +13,17 @@ main (void)
     tap.expect_eq (util::format::render ("%s", "identity"s), "identity"s, "identity string substitution");
     tap.expect_eq (util::format::render ("%s", "identity" ), "identity"s, "identity char[] substitution");
 
-    tap.expect_throw<util::format::missing_error> ([] (void) { util::format::render ("%s");     }, "missing value");
-    tap.expect_throw<util::format::format_error>  ([] (void) { util::format::render ("%!", 42); }, "invalid specifier");
-    tap.expect_throw<util::format::format_error>  ([] (void) { util::format::render ("%", 42);  }, "truncated specifier");
+    tap.expect_throw<util::format::missing_error> ([] (void) {
+        util::format::render ("%s");
+    }, "missing value");
+
+    tap.expect_throw<util::format::format_error> ([] (void) {
+        util::format::render ("%!", 42);
+    }, "invalid specifier");
+
+    tap.expect_throw<util::format::format_error> ([] (void) {
+        util::format::render ("%", 42);
+    }, "truncated specifier");
 
     return tap.status ();
 }
