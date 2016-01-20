6975529cfdd35a2f7b42f7afc1858e37ded13d13
Danny Robson <danny@nerdcruft.net>
2016-01-20 15:37:52 +1100

    log: update copyright dates

--- log.cpp
+++ log.cpp
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Copyright 2012 Danny Robson <danny@nerdcruft.net>
+ * Copyright 2012-2016 Danny Robson <danny@nerdcruft.net>
  */
 
 
