226e212826b4dbfbef89d3652622a3ba5ef64020
Danny Robson <danny@nerdcruft.net>
2016-02-10 14:05:44 +1100

    alloc/fwd: add forward decleration header

---
+++ alloc/fwd.hpp
@@ -0,0 +1,34 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2016 Danny Robson <danny@nerdcruft.net>
+ */
+
+#ifndef __UTIL_ALLOC_FWD_HPP
+#define __UTIL_ALLOC_FWD_HPP
+
+
+namespace util { namespace alloc {
+    class affix;
+    class dynamic;
+    class fallback;
+    class linear;
+    class malloc;
+    class null;
+    class stack;
+
+    template <typename T> class arena;
+    template <typename B, typename T> class allocator;
+} }
+
+#endif
