fe8bbd1b61c04e468c6f634130f42826322e902f
Danny Robson <danny@nerdcruft.net>
2016-07-28 13:45:44 +1000

    coord: add forward declarations

---
+++ coord/fwd.hpp
@@ -0,0 +1,27 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2016 Danny Robson <danny@nerdcruft.net>
+ */
+
+#ifndef __UTIL_COORD_FWD_HPP
+#define __UTIL_COORD_FWD_HPP
+
+namespace util {
+    template <size_t S, typename T> struct colour;
+    template <size_t S, typename T> struct extent;
+    template <size_t S, typename T> struct point;
+    template <size_t S, typename T> struct vector;
+}
+
+#endif
