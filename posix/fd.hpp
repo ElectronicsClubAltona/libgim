b949b90dd7f7a24014018f7617d5f30c80775fa3
Danny Robson <danny@nerdcruft.net>
2016-10-02 16:34:40 +1100

    posix/fd: add more comments

--- posix/fd.hpp
+++ posix/fd.hpp
@@ -25,20 +25,31 @@ namespace util::posix {
     /// A simple RAII wrapper for file descriptors
     class fd {
     public:
+        ///////////////////////////////////////////////////////////////////////
         fd (const char *path, int flags);
         fd (const char *path, int flags, mode_t);
 
         fd (fd &&);
+
+        // The int constructor steals the fd. So don't pass in something that
+        // you don't want closed at destruct time. This should really only be
+        // used when interfacing with results of syscalls that we don't wrap.
         explicit fd (int);
 
+        // copy constructors are removed in favour of explicit calls to dup.
+        // This should reduce unexpected or expensive copies as much as
+        // possible; one should not be doing this unless it is absolutely
+        // required.
         fd (const fd&) = delete;
         fd dup (void) const;
         static fd dup (int);
 
         ~fd ();
 
+        ///////////////////////////////////////////////////////////////////////
         struct ::stat stat (void) const;
 
+        ///////////////////////////////////////////////////////////////////////
         operator int (void) const;
 
     private:
