c0f3983ca890383ef4e48f4dc7477c0b399314b6
Danny Robson <danny@nerdcruft.net>
2017-01-09 15:54:47 +1100

    stream: add minimal scoped flag test case

---
+++ test/stream.cpp
@@ -0,0 +1,39 @@
+#include "tap.hpp"
+
+#include "stream.hpp"
+
+
+///////////////////////////////////////////////////////////////////////////////
+template <typename T>
+void
+test_scoped (util::TAP::logger&);
+
+
+//-----------------------------------------------------------------------------
+template <>
+void
+test_scoped<util::stream::scoped::flags> (util::TAP::logger &tap)
+{
+    std::ostringstream os;
+
+    {
+        util::stream::scoped::flags f (os);
+        os << std::hex;
+    }
+
+    os << 15;
+    
+    tap.expect_eq (os.str (), "15", "stream::scoped::flag reverts std::hex");
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+int
+main (int, char **)
+{
+    util::TAP::logger tap;
+
+    test_scoped<util::stream::scoped::flags> (tap);
+
+    return tap.status ();
+}
